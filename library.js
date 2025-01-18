f60458262d0f53e21596d99a9f7f24ff83368334
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-01-18 05:31:01 -0500

    Task 2 complete

--- library.js
+++ library.js
@@ -1,7 +1,55 @@
-class Book{
-    constructor(title, author, available = true){
-        this.title = title;
-        this.author = author;
-        this.available = available;
-    }
+class Book {
+  constructor(title, author, available = true) {
+    this.title = title;
+    this.author = author;
+    this.available = available;
+  }
 }
+const library = {
+  books: [],
+
+  addBook(title, author) {
+    newBook = new Book(title, author);
+    this.books.push(newBook);
+    console.log(
+      `Added "${newBook.title}" by ${newBook.author} to the library!\nThere are now ${this.books.length} books in the library's database.`
+    );
+  },
+  checkOutBook(title) {
+    let found = false;
+    for (let book of this.books) {
+      if (book.title === title) {
+        found = true;
+        if (book.available) {
+          book.available = false;
+          console.log(`Check out: ${book.title}`);
+        } else {
+          console.log(
+            `The book with title ${title} is not available at the moment!`
+          );
+        }
+
+        break;
+      }
+    }
+    if (!found) {
+      console.log(`The book with title ${title} is not found!`);
+    }
+  },
+
+  getAvailableBooks() {
+    //let availableBooks = this.books.filter((book) => book.available); using filter
+    let availableBooks = [];
+    for (let book of books) {
+      if (book.available) {
+        availableBooks.push(book.title);
+      }
+    }
+    console.log(
+      `There are ${
+        availableBooks.length
+      } titles currently on the shelf:\n${availableBooks.join(", ")}`
+    );
+    //return availableBooks;
+  },
+};
